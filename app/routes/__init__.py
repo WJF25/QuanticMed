ab8a15095074f6104cd44b8832a0dffe37461e41
Waldiney <walferreira@gmail.com>
2021-12-06 22:02:00 -0300

    chore: created new files and updated some names to plural version

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
-from app.routes.customer_blueprint import bp as bp_customer
-from app.routes.location_blueprint import bp as bp_location
-from app.routes.room_blueprint import bp as bp_room
-from app.routes.therapist_blueprint import bp as bp_therapist
-from app.routes.attendant_blueprint import bp as bp_attendant
+from app.routes.customers_blueprint import bp as bp_customer
+from app.routes.locations_blueprint import bp as bp_location
+from app.routes.rooms_blueprint import bp as bp_room
+from app.routes.therapists_blueprint import bp as bp_therapist
+from app.routes.attendants_blueprint import bp as bp_attendant
+from app.routes.specialities_blueprint import bp as bp_specialities
+from app.routes.sessions_blueprint import bp as bp_session
 
 
 def init_app(app: Flask):
@@ -13,3 +15,5 @@ def init_app(app: Flask):
     app.register_blueprint(bp_room)
     app.register_blueprint(bp_therapist)
     app.register_blueprint(bp_attendant)
+    app.register_blueprint(bp_specialities)
+    app.register_blueprint(bp_session)
