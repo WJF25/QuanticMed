fb3f350000d19a413e280e54c4e8e883de844bef
Rafael Leonardo <rafael.zoe26@gmail.com>
2021-12-07 11:34:39 -0300

    fix: english pattern Therapist.id

--- app/models/sessions_table_model.py
+++ app/models/sessions_table_model.py
@@ -6,8 +6,8 @@ db: sqlalchemy = sqlalchemy
 sessions = db.Table(
     id_consulta=db.Column(db.Integer),
     id_paciente=db.Column(db.Integer, db.ForeignKey(
-        'CustomerModel.id_paciente')),
-    id_medico=db.Column(db.Integer, db.ForeignKey('TherapistModel.id_medico')),
+        'Customer.id_customer')),
+    id_medico=db.Column(db.Integer, db.ForeignKey('Therapist.id_medic')),
     dt_inicio=db.Column(db.DateTime, nullable=False),
     dt_fim=db.Column(db.DateTime, nullable=False),
     ds_status=db.Column(db.String(15))
