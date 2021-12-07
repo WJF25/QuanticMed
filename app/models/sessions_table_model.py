1b6e1be77a80a718e3c60453dd47dd310a83faa0
Rafael Leonardo <rafael.zoe26@gmail.com>
2021-12-06 23:30:36 -0300

    feat: session table feita

--- app/models/sessions_table_model.py
+++ app/models/sessions_table_model.py
@@ -1,2 +1,15 @@
 from app.configs.database import db
 from dataclasses import dataclass
+import sqlalchemy
+db: sqlalchemy = sqlalchemy
+
+session = db.Table(
+    id_consulta=db.Column(db.Integer),
+    id_paciente=db.Column(db.Integer, db.ForeignKey(
+        'CustomerModel.id_paciente')),
+    id_medico=db.Column(db.Integer, db.ForeignKey('TherapistModel.id_medico')),
+    dt_inicio=db.Column(db.DateTime, nullable=False),
+    dt_fim=db.Column(db.DateTime, nullable=False),
+    ds_status=db.Column(db.String(15))
+
+)
