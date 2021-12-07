e7b11036928e6fe8f9e9532d76e7c7e6b063555d
Mgava <prgmgava@hotmail.com>
2021-12-07 10:23:34 -0300

    feat: class Rooms add

--- app/models/rooms_model.py
+++ app/models/rooms_model.py
@@ -1,2 +1,15 @@
 from app.configs.database import db
 from dataclasses import dataclass
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+@dataclass
+class Rooms(db.Model):
+    __tablename__ = "rooms"
+
+    nm_room: str
+    # backref para receber o nome da especialidade
+
+    id_room = Column(Integer, primary_key=True)
+    nm_room = Column(String(50), nullable=False)
+    id_specialty = Column(Integer, ForeignKey('Specialties.id_specialty'))
