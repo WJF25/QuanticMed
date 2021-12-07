0ed49d2f1024c9dd8a84a27a4b1d692ecefececb
Mgava <prgmgava@hotmail.com>
2021-12-07 10:59:48 -0300

    feat: relationship with speacilties add

--- app/models/rooms_model.py
+++ app/models/rooms_model.py
@@ -1,6 +1,7 @@
 from app.configs.database import db
 from dataclasses import dataclass
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 @dataclass
@@ -8,8 +9,10 @@ class Rooms(db.Model):
     __tablename__ = "rooms"
 
     nm_room: str
-    # backref para receber o nome da especialidade
+    # specialty: Specialties importar model
 
     id_room = Column(Integer, primary_key=True)
     nm_room = Column(String(50), nullable=False)
     id_specialty = Column(Integer, ForeignKey('Specialties.id_specialty'))
+
+    specialty = relationship("Specialties", uselist=False)
