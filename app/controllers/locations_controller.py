321f83f77b09227d4e527d5a38322ded0e6ce53d
Waldiney <walferreira@gmail.com>
2021-12-08 01:08:57 -0300

    feature: implemented and setted the functions for verifications of keys frm JSON

--- app/controllers/locations_controller.py
+++ app/controllers/locations_controller.py
@@ -1,6 +1,9 @@
 from flask import request, jsonify, current_app
+from app.exc.excessoes import WrongKeyError
 from app.models.locations_model import Locations
-import psycopg2
+from app.controllers.verifications import verify_keys
+from psycopg2.errors import UniqueViolation,NotNullViolation
+from sqlalchemy.exc import IntegrityError
 import sqlalchemy 
 
 
@@ -8,18 +11,26 @@ import sqlalchemy
 def create_location():
     session = current_app.db.session
 
-    data = request.get_json()
-    location = Locations(**data)
-    session.add(location)
-    session.commit()
-
-    response = dict(location)
-     
-    del response['clinic'], response['therapist']
+    try:
+        data = request.get_json()
+        verify_keys(data, "location", "post")
+        location = Locations(**data)
+        session.add(location)
+        session.commit()
         
-    
-    
-    return jsonify(response)
+
+        response = dict(location)
+    except WrongKeyError as error:
+        return jsonify({"Erro": error.value}), 400
+    except (IntegrityError ) as int_error:
+        if type(int_error.orig) == UniqueViolation:
+            return jsonify({"error": "Locação Já existe"}), 409
+        if type(int_error.orig) == NotNullViolation:
+            return jsonify({"erro": "Campo não pode ser nulo"}), 400
+
+    del response['clinic'], response['therapist']
+
+    return jsonify(response), 201
 
 
 def delete_location(location_id):
@@ -28,6 +39,8 @@ def delete_location(location_id):
     
     location = Locations.query.filter_by(id_location=location_id).first()
     response_location = dict(location)
+    if location is None:
+        return jsonify({"erro": "Locação não existe"}), 404
     session.delete(location)
     session.commit()
 
--- app/controllers/rooms_controller.py
+++ app/controllers/rooms_controller.py
@@ -1,4 +1,6 @@
 from flask import request, jsonify, current_app
+from app.controllers.verifications import verify_keys
+from app.exc.excessoes import WrongKeyError
 from app.models.rooms_model import Rooms
 import sqlalchemy 
 import psycopg2
@@ -7,10 +9,15 @@ import psycopg2
 
 def create_rooms():    
     session = current_app.db.session
-    data = request.get_json()
-    room = Rooms(**data)
-    session.add(room)
-    session.commit()
+
+    try:
+        data = request.get_json()
+        verify_keys(data, "room", "post")
+        room = Rooms(**data)
+        session.add(room)
+        session.commit()
+    except WrongKeyError as error:
+        return jsonify({"Erro": error.value}), 400
 
     return jsonify(room)
 
@@ -18,7 +25,7 @@ def create_rooms():
 def delete_room(room_id):
     session = current_app.db.session
     
-    
+
     room = Rooms.query.filter_by(id_room=room_id).first()
     romm_response = dict(room)
     if room is None:
