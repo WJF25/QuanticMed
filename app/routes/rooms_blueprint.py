056a05885df9c38fd8db6e46c3a7717324355219
Waldiney <walferreira@gmail.com>
2021-12-08 09:51:39 -0300

    feature: created patch for room

--- app/routes/rooms_blueprint.py
+++ app/routes/rooms_blueprint.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 
-from app.controllers.rooms_controller import create_rooms, delete_room
+from app.controllers.rooms_controller import create_rooms, delete_room, update_room
 
 
 bp = Blueprint('room_bp', __name__, url_prefix='/rooms')
 
 bp.post("")(create_rooms)
 bp.delete("/<int:room_id>")(delete_room)
+bp.patch("/<int:room_id>")(update_room)
