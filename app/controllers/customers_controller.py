4d3caa6033d054809ef54a81b3dc0787f886a88f
Rafael Leonardo <rafael.zoe26@gmail.com>
2021-12-08 02:19:40 -0300

    feat: customer crud completed

--- app/controllers/customers_controller.py
+++ app/controllers/customers_controller.py
@@ -1,7 +1,9 @@
-from flask import request, jsonify, current_app
+from flask import json, request, jsonify, current_app
 from psycopg2.errors import UniqueViolation, NotNullViolation
 from sqlalchemy.exc import IntegrityError
+from app.exc.customers_errors import CustomerInvalidCpf, CustomerNotFoundError
 from app.models.customers_model import Customers
+import re
 
 
 def create_customer():
@@ -26,10 +28,18 @@ def create_customer():
     keys = list(customer_data.keys())
     Customers.get_missing_keys(keys, valid_keys, missing_keys)
     customer = Customers(**customer_data)
+    cpf = customer_data["nr_cpf"]
+    rg = customer_data["nr_rg"]
+    try:
+        if len(cpf) != 11 or len(rg) != 11:
+            raise CustomerInvalidCpf({"msg": "cpf and rg field must have 11 digits"})
+    except CustomerInvalidCpf as E:
+        return jsonify(E.value)
     try:
         session.add(customer)
         session.commit()
-        return jsonify(customer), 200
+
+        return jsonify(customer), 201
     except IntegrityError as E:
         if isinstance(E.orig, UniqueViolation):
             return {"msg": "Customer already exists in the database"}, 409
@@ -39,10 +49,10 @@ def create_customer():
                 "valid_keys": valid_keys,
                 "missing_keys": missing_keys,
             }, 400
-    return {"msg": "I'm sorry, we did not understand your request"}
+    return {"msg": "I'm sorry, we did not understand your request"}, 400
 
 
-def update_customer_by_id(customer_id):
+def update_customer_by_id(id_customer):
     valid_keys = [
         "nm_customer",
         "nr_cpf",
@@ -61,3 +71,31 @@ def update_customer_by_id(customer_id):
     request_keys = list(data_to_update.keys())
     Customers.delete_invalid_keys(request_keys, valid_keys, data_to_update)
     new_keys = list(data_to_update.keys())
+    if len(new_keys):
+        return {"message": "Invalid Body request"}, 400
+    try:
+        customer_found = Customers.query.filter_by(id_customer=id_customer).update(
+            data_to_update
+        )
+        if not customer_found:
+            raise CustomerNotFoundError({"msg": "customer not found"})
+        session.commit()
+        new_costumer = Customers.query.get(id_customer)
+        return jsonify(new_costumer), 200
+    except CustomerNotFoundError as E:
+        return jsonify(E.value), 404
+
+
+def delete_customer_by_id(id_customer):
+    session = current_app.db.session
+    try:
+        cate_to_deleted = Customers.query.filter_by(
+            id_customer=id_customer
+        ).one_or_none()
+        if not cate_to_deleted:
+            raise CustomerNotFoundError({"msg": "customer not found"})
+        session.delete(cate_to_deleted)
+        session.commit()
+        return "", 204
+    except CustomerNotFoundError as E:
+        return jsonify(E.value), 404
