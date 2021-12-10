7ccf97de818ef2be872fab491fd303effbbf6b94
Waldiney <walferreira@gmail.com>
2021-12-10 01:26:25 -0300

    feature:created endpoint get for to bring room's schedule

--- app/controllers/rooms_controller.py
+++ app/controllers/rooms_controller.py
@@ -3,8 +3,11 @@ from app.controllers.verifications import verify_keys, verify_none_values
 from app.exc.excessoes import WrongKeyError, NoExistingValueError
 from app.models.rooms_model import Rooms
 from sqlalchemy.exc import DataError, IntegrityError
-import sqlalchemy
+from sqlalchemy import desc, asc
 from psycopg2.errors import ForeignKeyViolation
+from app.models.sessions_model import Sessions
+from app.models.therapists_model import Therapists
+from app.models.locations_model import Locations
 
 
 
@@ -56,7 +59,7 @@ def update_room(room_id):
         verify_keys(data, "room", "patch")
         current_room = Rooms.query.filter_by(id_room=room_id).first()
         verify_none_values(current_room)
-        data["nm_room"] = data["nm_room"].title()
+        
         updated_room = session.query(Rooms).filter_by(id_room=room_id).update(data)
         session.commit()
     except WrongKeyError as error:
@@ -75,10 +78,18 @@ def update_room(room_id):
 def get_rooms():
     session = current_app.db.session
     param:dict = dict(request.args)
+
+    q_options={
+        "dsc": desc(getattr(Rooms, param.get('order_by', 'id_room'))),        
+        "asc": asc(getattr(Rooms, param.get('order_by', 'id_room')))     
+    }
     
     
     if param:
-        ordered_rooms = session.query(Rooms).order_by(getattr(Rooms, param['data'])).paginate(int(param.get('page',1)),int(param.get('per_page',10)), max_per_page=20).items
+        ordered_rooms = session.query(Rooms)\
+        .order_by(q_options.get(param.get("dir", "asc")))\
+        .paginate(int(param.get('page', 1)), int(param.get('per_page',10)), max_per_page=20).items
+
         response = [dict(room) for room in ordered_rooms]
         return jsonify(response)
 
@@ -103,10 +114,27 @@ def get_room_by_id(room_id):
 
 
 def get_room_by_status(room_status):
-    session = current_app.db.session
+    
+    param:dict = dict(request.args)
 
-    room = Rooms.query.filter(Rooms.ds_status.ilike('%' + room_status + '%')).all()
+    q_options={
+        "dsc": desc(getattr(Rooms, param.get('order_by', 'id_room'))),
+        "asc": asc(getattr(Rooms, param.get('order_by', 'id_room')))
+    }
+    
+    room = Rooms.query.filter(Rooms.ds_status.ilike('%' + room_status + '%')).order_by(q_options.get(param.get('dir', 'asc'))).all()
     if room is None:
         return jsonify({"erro": "Sala não existe"}), 404
+
+    return jsonify(room), 200
+
+
+def get_room_schedule(id_room):
+
+    param:dict = dict(request.args)
+    print(param)
+    room = Sessions.query.select_from(Sessions).join(Therapists).join(Locations).join(Rooms).filter(Rooms.id_room == id_room).filter(Sessions.ds_status==param.get("status_consulta", "agendado")).all()
+
+    print(room)
 
     return jsonify(room), 200
